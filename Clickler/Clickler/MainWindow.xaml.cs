bb2ec2968496cda720e43d14c75d4c00296057c8
Fábián Máté <f.mate2004@gmail.com>
2022-05-31 12:47:35 +0200

    update2

--- Clickler/Clickler/MainWindow.xaml.cs
+++ Clickler/Clickler/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.IO;
 
 namespace Clickler
@@ -22,6 +25,7 @@ namespace Clickler
     public partial class MainWindow : Window
     {
         public int szamlalo;
+        public int szorzo=1;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,39 +33,41 @@ namespace Clickler
 
         public void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!Directory.Exists(@"C:\Users\tanulo\AppData\Roaming\FabiFolder\"))
+
+            if (!Directory.Exists(@"C:\FabiFolder\"))
             {
-                Directory.CreateDirectory(@"C:\Users\tanulo\AppData\Roaming\FabiFolder\");
+                Directory.CreateDirectory(@"C:\FabiFolder\");
             }
-            if (File.Exists(@"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt"))
+            if (File.Exists(@"C:\FabiFolder\szamlaloadat.txt"))
             {
 
             }
             else
             {
-                StreamWriter sw = new StreamWriter(@"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt");
+                StreamWriter sw = new StreamWriter(@"C:\FabiFolder\szamlaloadat.txt");
                 sw.WriteLine("0");
                 sw.Close();
             }
+            MusicCopy();
 
-            Szamlalo.Content = System.IO.File.ReadAllText(@"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt");
+            Szamlalo.Content = System.IO.File.ReadAllText(@"C:\FabiFolder\szamlaloadat.txt");
             szamlalo = Convert.ToUInt16(Szamlalo.Content);
 
             hatterkep.Visibility = Visibility.Hidden;
 
             kinalat.Items.Add("Háttér");
             kinalat.Items.Add("Zene");
-            kinalat.Items.Add("Autoclicker");
-            kinalat.Items.Add("Hangeffekt");
+            kinalat.Items.Add("Duplázás");
+            kinalat.Items.Add("Triplázás");
 
         }
 
         public void Button_Click(object sender, RoutedEventArgs e)
         {
-            szamlalo++;
+            szamlalo+=szorzo;
             Szamlalo.Content = szamlalo;
             var mentestartalma = Szamlalo.Content;
-            var fajlnev = @"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt";
+            var fajlnev = @"C:\FabiFolder\szamlaloadat.txt";
 
             using (StreamWriter sw = File.CreateText(fajlnev))
             {
@@ -86,7 +92,7 @@ namespace Clickler
                     szamlalo = szamlalo - 1000;
                     Szamlalo.Content = szamlalo;
                     var mentestartalma = Szamlalo.Content;
-                    var fajlnev = @"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt";
+                    var fajlnev = @"C:\FabiFolder\szamlaloadat.txt";
 
                     using (StreamWriter sw = File.CreateText(fajlnev))
                     {
@@ -107,7 +113,8 @@ namespace Clickler
                 else
                 {
                     MediaPlayer zene = new MediaPlayer();
-                    zene.Open(new Uri(@"/Muzsika.wav"));
+                    zene.Open(new Uri(@"C:\FabiFolder\Muzsika.wav"));
+                    MessageBox.Show("");
                     zene.Play();
 
 
@@ -116,7 +123,7 @@ namespace Clickler
                     szamlalo = szamlalo - 2000;
                     Szamlalo.Content = szamlalo;
                     var mentestartalma = Szamlalo.Content;
-                    var fajlnev = @"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt";
+                    var fajlnev = @"C:\FabiFolder\szamlaloadat.txt";
 
                     using (StreamWriter sw = File.CreateText(fajlnev))
                     {
@@ -125,19 +132,24 @@ namespace Clickler
                     }
                 }
 
-                if (Convert.ToInt32(Szamlalo.Content) < 2000)
+
+            }
+
+            else if(kinalat.SelectedIndex==2)
+            {
+                if (Convert.ToInt32(Szamlalo.Content) < 5000)
                 {
-                    MessageBox.Show("Nincs elég clicked! Az autoclicker ára 5000 pont!");
+                    MessageBox.Show("Nincs elég clicked! A duplázás ára 5000 pont!");
                 }
                 else
                 {
-
+                    szorzo = 2;
                     kinalat.Items.RemoveAt(2);
                     szamlalo = Convert.ToInt32(Szamlalo.Content);
                     szamlalo = szamlalo - 5000;
                     Szamlalo.Content = szamlalo;
                     var mentestartalma = Szamlalo.Content;
-                    var fajlnev = @"C:\Users\tanulo\AppData\Roaming\FabiFolder\szamlaloadat.txt";
+                    var fajlnev = @"C:\FabiFolder\szamlaloadat.txt";
 
                     using (StreamWriter sw = File.CreateText(fajlnev))
                     {
@@ -146,6 +158,36 @@ namespace Clickler
                     }
                 }
             }
+
+            else if (kinalat.SelectedIndex == 3)
+            {
+                if (Convert.ToInt32(Szamlalo.Content) < 20000)
+                {
+                    MessageBox.Show("Nincs elég clicked! A triplázás ára 20000 pont!");
+                }
+                else
+                {
+                    szorzo = 3;
+                    kinalat.Items.RemoveAt(3);
+                    szamlalo = Convert.ToInt32(Szamlalo.Content);
+                    szamlalo = szamlalo - 20000;
+                    Szamlalo.Content = szamlalo;
+                    var mentestartalma = Szamlalo.Content;
+                    var fajlnev = @"C:\FabiFolder\szamlaloadat.txt";
+
+                    using (StreamWriter sw = File.CreateText(fajlnev))
+                    {
+                        sw.Write(mentestartalma);
+                        sw.Close();
+                    }
+                }
+            }
+
         }
+        public void MusicCopy()
+        {
+            File.Copy("Muzsika.wav", @"C:\FabiFolder\Muzsika.wav", true);
+        }
+
     }
 }
